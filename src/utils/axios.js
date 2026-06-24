f45e2642db54cff44810d1734a5ee9cbe8481f5b
JalilovAbdurahmon <jalilovabdurahmon2010@gmail.com>
2026-06-24 19:42:19 +0500

    updated axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const instance = axios.create({
-  baseURL: "http://localhost:7000",
+  baseURL: "novita-backend-production.up.railway.app",
   headers: { "Content-Type": "application/json" },
 });
 

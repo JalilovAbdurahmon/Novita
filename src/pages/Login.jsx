a8f8d681cd28181b1873ffec589dc8fe584c3c33
JalilovAbdurahmon <jalilovabdurahmon2010@gmail.com>
2026-06-24 20:31:25 +0500

    updated login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { scheduleTokenExpiry } from "../utils/axios";
 
-const API_URL = "http://localhost:7000";
+const API_URL = "http://novita-backend-production.up.railway.app";
 
 const Login = () => {
   const navigate = useNavigate();
